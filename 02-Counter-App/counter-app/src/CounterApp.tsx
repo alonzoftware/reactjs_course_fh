26a0e0a29df96a49a8a2720e4683d6d7cffe1f69
SonyLap <alonzoftware@gmail.com>
2022-07-19 09:00:28 -0400

    SonyLap 20220719 09:00 02-Counter-App End Section

---
+++ 02-Counter-App/counter-app/src/CounterApp.tsx
@@ -0,0 +1,25 @@
+
+import { useState } from "react";
+
+export const CounterApp = ({ value = 0 }) => {
+    const [counter, setCounter] = useState(value);
+    const handleButtonAdd = () => {
+        // setCounter(counter + 1);
+        setCounter((c) => c + 1);
+    }
+    const handleButtonSubstract = () => {
+        setCounter(counter - 1);
+    }
+    const handleButtonReset = () => {
+        setCounter(value);
+    }
+    return (
+        <>
+            <h1>Counter App</h1>
+            <h5> {counter} </h5>
+            <button onClick={handleButtonAdd}> +1 </button>
+            <button onClick={handleButtonSubstract}> -1 </button>
+            <button onClick={handleButtonReset}> RESET </button>
+        </>
+    )
+}
--- 02-Counter-App/counter-app/src/main.tsx
+++ 02-Counter-App/counter-app/src/main.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { CounterApp } from "./CounterApp";
 import { FirstApp } from "./FirstApp";
 import { App } from "./HelloWorldApp";
 
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
+    // <React.StrictMode>
+    //     <FirstApp title="Hola Soy Goku" subTitle={123} />
+    // </React.StrictMode>
     <React.StrictMode>
-        <FirstApp title="Hola Soy Goku" subTitle={123} />
+        <CounterApp value={10} />
     </React.StrictMode>
 )
