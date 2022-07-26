b8a2b316a89ad79016d177de066f3efec6de725c
AsusPC <alonzoftware@gmail.com>
2022-07-26 13:01:59 -0400

    AsusPC 20220726 13:00 04-GifExpertApp End AddCategory func component

--- 04-GifExpertApp/gif-expert-lon/src/GifExpertApp.tsx
+++ 04-GifExpertApp/gif-expert-lon/src/GifExpertApp.tsx
@@ -1,10 +1,36 @@
+import { useState } from "react"
+import { AddCategory } from "./components/AddCategory";
+
+
+
 export const GifExpertApp = () => {
+    const iniValue: string[] = [];
+    const [category, setCategory] = useState(iniValue);
+
+    const onAddCategory = (newCategory: string) => {
+        if (newCategory != "" && !category.includes(newCategory)) {
+            setCategory([newCategory, ...category]);
+        }
+
+    }
     return (
         <>
             {/* title*/}
             <h1>GifExpertApp</h1>
             {/* input*/}
+            <AddCategory
+                // setCategory={setCategory} 
+                onNewCategory={(newCategory) => onAddCategory(newCategory)}
+            />
+            {/* <button onClick={onAddCategory}>Add Category</button> */}
             {/* Gif List*/}
+            <ol>
+                {category.map(category => {
+                    return (
+                        <li key={category}>{category}</li>)
+                })}
+
+            </ol>
             {/* Gif Item*/}
         </>
     )
---
+++ 04-GifExpertApp/gif-expert-lon/src/components/AddCategory.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+interface iProps {
+    // setCategory: React.Dispatch<React.SetStateAction<string[]>>;
+    onNewCategory(newCategory: string): void;
+}
+// export const AddCategory = ({ onNewCategory = (newCategory: string) => { } }) => {
+export const AddCategory = (props: iProps) => {
+    // const { setCategory} = props;
+    const { onNewCategory } = props;
+    const [inputValue, setInputValue] = useState('');
+    const onInputChange = (value = "") => {
+        setInputValue(value);
+    }
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (inputValue.trim().length <= 1) return
+        // setCategory(category => [inputValue, ...category]);
+        setInputValue("");
+        onNewCategory(inputValue.trim());
+    }
+    return (
+        <form onSubmit={event => onSubmit(event)}>
+
+            <input
+                type="text"
+                placeholder="Your Anime"
+                value={inputValue}
+                onChange={event => onInputChange(event.target.value)}
+            />
+        </form>
+
+
+    )
+
+
+}
+
