483f40d8d74c4e6d6e2f7004a7a5cb17e35bda42
ToshiLap <alonzoftware@gmail.com>
2022-09-20 21:46:37 -0400

    ToshiLap 20220920 21:45 05-HookApp :: test TodoItem

---
+++ 05-HookApp/hook-app-lon/tests/08-useReducer/Todo-Item.test.tsx
@@ -0,0 +1,20 @@
+
+import { iTodo } from "../../src/08-useReducer/TodoList";
+
+describe('Test <TodoItem />', () => {
+    const todo : iTodo = {
+        id: 1,
+        descrip : 'Piedra del Alma',
+        done:false
+    }
+    const onDelItemMock = jest.fn();
+    const onToggleItemMock = jest.fn();
+
+    beforeEach(()=>jest.clearAllMocks());
+
+    test('should show pending TODO', () => { 
+
+        
+     })
+
+ })
