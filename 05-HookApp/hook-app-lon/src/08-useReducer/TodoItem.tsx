7430e7f568d63d491812fec5102aa6b37b5f89ca
AsusPC <alonzoftware@gmail.com>
2022-09-21 10:32:07 -0400

    AsusPC 20220921 10:30 05-HookApp :: End Test TodoItem

--- 05-HookApp/hook-app-lon/src/08-useReducer/TodoItem.tsx
+++ 05-HookApp/hook-app-lon/src/08-useReducer/TodoItem.tsx
@@ -11,7 +11,7 @@ export const TodoItem = ({ id, descrip, done, onDelItem, onToggleItem }: iProps)
     return (
         <li className='list-group-item d-flex justify-content-between'>
 
-            <span className={`align-self-center ${done ? 'text-decoration-line-through' : ''}`}
+            <span aria-label="span" className={`align-self-center ${done ? 'text-decoration-line-through' : ''}`}
                 onClick={() => onToggleItem(id)}
             >{descrip}</span>
             <button className='btn btn-danger'
--- 05-HookApp/hook-app-lon/tests/08-useReducer/Todo-Item.test.tsx
+++ 05-HookApp/hook-app-lon/tests/08-useReducer/Todo-Item.test.tsx
@@ -1,5 +1,8 @@
 
+import { fireEvent, render,screen } from "@testing-library/react";
 import { iTodo } from "../../src/08-useReducer/TodoList";
+import { TodoItem } from "../../src/08-useReducer/TodoItem";
+import React from "react";
 
 describe('Test <TodoItem />', () => {
     const todo : iTodo = {
@@ -7,14 +10,63 @@ describe('Test <TodoItem />', () => {
         descrip : 'Piedra del Alma',
         done:false
     }
-    const onDelItemMock = jest.fn();
-    const onToggleItemMock = jest.fn();
+    const onToggleItemMock : Function = jest.fn();
+    const onDelItemMock : Function= jest.fn();
 
     beforeEach(()=>jest.clearAllMocks());
 
     test('should show pending TODO', () => { 
-
+        render(<TodoItem 
+            {...todo}
+            onDelItem={onDelItemMock}
+            onToggleItem={onToggleItemMock}
+            />);
+        //screen.debug();
+        const liElement = screen.getByRole('listitem');
+        // console.log(liElement.innerHTML);
+        expect(liElement.className).toBe('list-group-item d-flex justify-content-between');
+        const spanElement = screen.getByLabelText('span');
+        //console.log(spanElement.className);
+        expect(spanElement.className).toBe('align-self-center '); //With space
+        expect(spanElement.className).toContain('align-self-center');
         
      })
+    test('should show done TODO', () => { 
+        todo.done = true;
+        render(<TodoItem 
+            {...todo}
+            onDelItem={onDelItemMock}
+            onToggleItem={onToggleItemMock}
+            />);
+        const spanElement = screen.getByLabelText('span');
+        //console.log(spanElement.className);
+        expect(spanElement.className).toBe('align-self-center text-decoration-line-through');
+     })
+    test('onToggleItem have been Called', () => { 
+
+        render(<TodoItem 
+            {...todo}
+            onDelItem={onDelItemMock}
+            onToggleItem={onToggleItemMock}
+            />);
+        const spanElement = screen.getByLabelText('span');
+        //console.log(spanElement.className);
+        fireEvent.click(spanElement);
+        expect(onToggleItemMock).toHaveBeenCalledWith(todo.id);
+     })
+    test('onDelItem have been Called', () => { 
+
+        render(<TodoItem 
+            {...todo}
+            onDelItem={onDelItemMock}
+            onToggleItem={onToggleItemMock}
+            />);
+        const buttonElement = screen.getByRole('button');
+        //console.log(spanElement.className);
+        fireEvent.click(buttonElement);
+        expect(onDelItemMock).toHaveBeenCalledWith(todo.id);
+     })
+
+
 
  })
