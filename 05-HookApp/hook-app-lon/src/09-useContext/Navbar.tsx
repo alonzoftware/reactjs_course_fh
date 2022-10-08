e0b83f1b9fef6149df0a7274d38a6ebcef4cdb45
AsusLap <alonzoftware@gmail.com>
2022-10-08 02:52:31 -0400

    AsusLap 20221008 02:51 05-HookApp :: Improve Navbar UserContext

--- 05-HookApp/hook-app-lon/src/09-useContext/Navbar.tsx
+++ 05-HookApp/hook-app-lon/src/09-useContext/Navbar.tsx
@@ -2,9 +2,9 @@ import { Link, NavLink } from "react-router-dom"
 
 export const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark rounded-3">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <Link className="navbar-brand" to="/">Navbar</Link>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
