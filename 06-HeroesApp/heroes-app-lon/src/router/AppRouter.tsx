e5155dd6cb5fdee8b97e3a64c4c777f5f7dd7bb7
SonyLap <alonzoftware@gmail.com>
2022-10-14 10:16:08 -0400

    SonyLap 20221014 10:15 06-HeroesApp :: Routers

--- 06-HeroesApp/heroes-app-lon/src/router/AppRouter.tsx
+++ 06-HeroesApp/heroes-app-lon/src/router/AppRouter.tsx
@@ -1,17 +1,17 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Navbar } from '../ui';
-import { LoginPage } from "../auth/pages";
-import { DcPage, MarvelPage } from "../heroes/pages/";
+import { LoginPage } from "../auth";
+import { HeroesRoutes } from '../heroes/routes/HeroesRoutes';
 
 export const AppRouter = () => {
     return (
         <>
-            <Navbar />
             <Routes>
-                <Route path="/" element={<Navigate to='/login' />} />
+                {/* <Route path="/" element={<Navigate to='/login' />} /> */}
                 <Route path="login" element={<LoginPage />} />
-                <Route path="marvel" element={<MarvelPage />} />
-                <Route path="dc" element={<DcPage />} />
+                <Route path="/*" element={<HeroesRoutes />} />
+                {/* <Route path="marvel" element={<MarvelPage />} /> */}
+                {/* <Route path="dc" element={<DcPage />} /> */}
             </Routes>
 
 
