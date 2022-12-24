c6dd23e957d7252c12ec0bd3cf6a107d0349bbc2
ToshiLap <alonzoftware@gmail.com>
2022-12-23 21:54:23 -0400

    ToshiLap 20221223 21:50 07-MaterialUI :: Views

--- 07-MaterialUI/material-ui-lon/src/journal/pages/JournalPage.tsx
+++ 07-MaterialUI/material-ui-lon/src/journal/pages/JournalPage.tsx
@@ -1,13 +1,16 @@
 import { Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
+import { NothingSelectedView } from "../views/NothingSelectedView";
 // import MailOutline from "@mui/icons-material/MailOutline";
 
 export const JournalPage = () => {
     return (
         <JournalLayout>
             {/* <Typography variant='h1'>JournalPage</Typography> */}
-            <Typography>Eu sint cupidatat consectetur in irure magna labore consectetur ex dolor sunt minim consectetur. Ut dolor tempor consectetur laborum qui amet ea eu. Qui nulla mollit excepteur sint ad sint est.</Typography>
+            {/* <Typography>Eu sint cupidatat consectetur in irure magna labore consectetur ex dolor sunt minim consectetur. Ut dolor tempor consectetur laborum qui amet ea eu. Qui nulla mollit excepteur sint ad sint est.</Typography> */}
             {/* <MailOutline /> */}
+
+            <NothingSelectedView />
         </JournalLayout >
     )
 }
