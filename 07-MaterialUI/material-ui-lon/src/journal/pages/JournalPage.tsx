84a2d3a21a997ff7ecdee48adaba23660a61d2ee
SonyLap <alonzoftware@gmail.com>
2022-12-24 12:17:55 -0400

    SonyLap 20221224 12:17 07-MaterialUI :: End Section

--- 07-MaterialUI/material-ui-lon/src/journal/pages/JournalPage.tsx
+++ 07-MaterialUI/material-ui-lon/src/journal/pages/JournalPage.tsx
@@ -1,5 +1,7 @@
-import { Typography } from "@mui/material";
+import { AddOutlined } from "@mui/icons-material";
+import { IconButton, Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
+import { NoteView } from "../views";
 import { NothingSelectedView } from "../views/NothingSelectedView";
 // import MailOutline from "@mui/icons-material/MailOutline";
 
@@ -11,6 +13,20 @@ export const JournalPage = () => {
             {/* <MailOutline /> */}
 
             <NothingSelectedView />
+            {/* <NoteView /> */}
+
+            <IconButton
+                size='large'
+                sx={{
+                    color: 'white',
+                    backgroundColor: 'error.main',
+                    ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+                    position: 'fixed',
+                    right: 50,
+                    bottom: 50
+                }}>
+                <AddOutlined sx={{ fontSize: 30 }} />
+            </IconButton>
         </JournalLayout >
     )
 }
