3db092e7a8953eb13fcd214d2e3310f3293db6de
ToshiLap <alonzoftware@gmail.com>
2023-01-11 21:52:38 -0400

    ToshiLap 20230111 21:51 08-ToolkitRedux :: Use RTKQuery

--- 08-ToolkitRedux/toolkit-redux-lon/src/TodoApp.tsx
+++ 08-ToolkitRedux/toolkit-redux-lon/src/TodoApp.tsx
@@ -1,11 +1,31 @@
-
+import { useGetTodosQuery } from "./store/rtk_apis";
+interface iTodo {
+    userId: number,
+    id: number,
+    title: string,
+    completed: boolean
+}
 export const TodoApp = () => {
+
+    const { data: todos = [], isLoading } = useGetTodosQuery();
     return (
         <>
             <h1>Todos - RTK Query</h1>
             <hr />
-            <h4>isLoading ...</h4>
+            <h4>isLoading: {isLoading ? 'TRUE' : 'FALSE'}</h4>
             <pre>...</pre>
+            <ul>
+                {todos.map(todo => (
+                    <li key={todo.id}>
+                        <strong>{todo.completed ? 'DONE' : 'PENDING'}</strong>
+                        {todo.title}
+                    </li>))}
+
+
+            </ul>
+
+
+
             <button>Next Todo</button>
         </>
     )
--- 08-ToolkitRedux/toolkit-redux-lon/src/store/store.ts
+++ 08-ToolkitRedux/toolkit-redux-lon/src/store/store.ts
@@ -1,12 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { counterSlice } from './slices/counter';
 import { pokemonSlice } from './slices/pokemon';
+import { todosApi } from './rtk_apis/todosApi';
 
 export const store = configureStore({
     reducer: {
         counter: counterSlice.reducer,
-        pokemons: pokemonSlice.reducer
+        pokemons: pokemonSlice.reducer,
+        [todosApi.reducerPath]: todosApi.reducer,
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(todosApi.middleware)
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
