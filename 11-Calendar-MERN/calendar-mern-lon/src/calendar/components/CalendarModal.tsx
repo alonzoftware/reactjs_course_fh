0c9452e471f11ca4305ea6002781d25379617642
SonyLap <alonzoftware@gmail.com>
2023-08-23 10:39:08 -0400

    HPLap 20230823 10:38 11-Calendar-Mern :: DatePicker

--- 11-Calendar-MERN/calendar-mern-lon/src/calendar/components/CalendarModal.tsx
+++ 11-Calendar-MERN/calendar-mern-lon/src/calendar/components/CalendarModal.tsx
@@ -1,5 +1,11 @@
+import { addHours } from 'date-fns';
 import { useState } from 'react';
 import Modal from 'react-modal';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { registerLocale, setDefaultLocale } from "react-datepicker";
+import es from 'date-fns/locale/es';
+registerLocale('es', es)
 const customStyles = {
     content: {
         top: '50%',
@@ -16,10 +22,37 @@ Modal.setAppElement('#root');
 export const CalendarModal = () => {
     const [isOpen, setIsOpen] = useState(true);
 
+    const [formValues, setFormValues] = useState({
+        title: 'Alonzo',
+        notes: 'Choque',
+        start: new Date(),
+        end: addHours(new Date(), 2),
+    })
+
     const onCloseModal = () => {
         console.log('Cerrando Modal');
         setIsOpen(false);
 
+    }
+
+    // const onInputChange = (event: React.ChangeEvent<HTMLInputElement> | React.ChangeEventHandler<HTMLTextAreaElement>) => {
+    const onInputChange = (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+        const { target } = event;
+        const { name, value } = target;
+
+        setFormValues({
+            ...formValues,
+            [name]: value,
+        })
+
+    }
+    const onDateChanged = (event: Date | null, changing: string) => {
+        setFormValues(
+            {
+                ...formValues,
+                [changing]: event
+            }
+        )
     }
 
     return (
@@ -32,9 +65,74 @@ export const CalendarModal = () => {
             overlayClassName="modal-fondo"
             closeTimeoutMS={200}
         >
-            <h1>Hola Mundo</h1>
+            <h1> Nuevo Evento </h1>
             <hr />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem consectetur nam rem consequuntur</p>
+            <form className="container">
+
+                <div className="form-group mb-2">
+                    <label>Fecha y hora inicio</label><br />
+                    {/* <input className="form-control" placeholder="Fecha inicio" /> */}
+                    <DatePicker
+                        locale="es"
+                        selected={formValues.start}
+                        onChange={(event) => onDateChanged(event, 'start')} //only when value has changed
+                        className="form-control"
+                        showTimeSelect
+                        dateFormat="Pp"
+                    />
+                </div>
+
+                <div className="form-group mb-2">
+                    <label>Fecha y hora fin</label><br />
+                    {/* <input className="form-control" placeholder="Fecha inicio" /> */}
+                    <DatePicker
+                        locale="es"
+                        minDate={formValues.start}
+                        selected={formValues.end}
+                        onChange={(event) => onDateChanged(event, 'end')}//only when value has changed
+                        className="form-control"
+                        showTimeSelect
+                        dateFormat="Pp"
+                    />
+                </div>
+
+                <hr />
+                <div className="form-group mb-2">
+                    <label>Titulo y notas</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Título del evento"
+                        name="title"
+                        autoComplete="off"
+                        value={formValues.title}
+                        onChange={onInputChange}
+                    />
+                    <small id="emailHelp" className="form-text text-muted">Una descripción corta</small>
+                </div>
+
+                <div className="form-group mb-2">
+                    <textarea
+                        // type="text"
+                        className="form-control"
+                        placeholder="Notas"
+                        rows={5}
+                        name="notes"
+                        value={formValues.notes}
+                        onChange={onInputChange}
+                    ></textarea>
+                    <small id="emailHelp" className="form-text text-muted">Información adicional</small>
+                </div>
+
+                <button
+                    type="submit"
+                    className="btn btn-outline-primary btn-block"
+                >
+                    <i className="far fa-save"></i>
+                    <span> Guardar</span>
+                </button>
+
+            </form>
         </Modal>
     )
 }
